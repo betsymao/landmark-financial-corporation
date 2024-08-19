de6a0ba07a42f67e1aba74f1f67d455d16d64ddc
Betsy <132311670+betsymao@users.noreply.github.com>
2024-08-19 19:02:56 +1000

    added layout component

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./navbar";
+import Footer from "./footer";
+
+function layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default layout;
