7a5bbbb22c2914a81ed53872655f7d20144642e0
Betsy <132311670+betsymao@users.noreply.github.com>
2024-08-20 14:51:51 +1000

    home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,15 @@
+import Hero from "../../components/home/hero";
+import Services from "../../components/home/services";
+import About from "../../components/home/about";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <About />
+    </>
+  );
+}
+
+export default Home;
